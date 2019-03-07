707c5ef6b305bf750b670b1a6ace0455d119698b
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-07 14:02:58 +0530

    My First Test Case

---
+++ src/components/testingComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TestingComponent() {
+    return <h1>Hello World</h1>;
+}
---
+++ src/tests/renderTestComponent.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestingComponent from "../components/testingComponent";
+
+it("should Render Component", () => {
+    const divElement = document.createElement("div");
+    ReactDOM.render(<TestingComponent />, divElement);
+})
