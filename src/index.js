5b017bf49012b89844ced948d0cbaad6bf669b41
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-06-06 16:58:56 +0530

    Asynchronous Programmimg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ParentReturnFalse from "./components/shouldComponentUpdate/usingObjectsFromParent/parentReturnFalse";
+import AsynchUpdates from "./components/updatesAreAsynchronous/asynchUpdates";
 
 
-ReactDOM.render(<ParentReturnFalse />, document.getElementById('root'));
+ReactDOM.render(<AsynchUpdates />, document.getElementById('root'));
 
