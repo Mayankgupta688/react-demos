69e5c0e2802f40ce37626229fb0b42fe6c56a75d
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2019-07-21 14:06:42 +0530

    Changes

--- src/components/sessionCode/components/childStatePropogationClass.jsx
+++ src/components/sessionCode/components/childStatePropogationClass.jsx
@@ -5,18 +5,20 @@ export default class ChildStatePropogationClass extends React.Component {
         super();
         this.state = {
             name: "Mayank",
-            age: 30,
             designation: "Developer"
         }
 
-        this.updateNameValues = this.updateNameValues.bind(this)
-
         setTimeout(() => {
             this.setState({
-                name: "Anshul",
-                age: 50
+                name: "Anshul"
             })
         }, 4000);
+    }
+
+    updateName = () => {
+        this.setState({
+            name: "SXYSmccklsf"
+        })
     }
 
     
@@ -24,25 +26,43 @@ export default class ChildStatePropogationClass extends React.Component {
         return (
             <div id="mainDiv"> 
                 <b>This is Parent Component</b><hr></hr><br></br>
-                <ChildComponent name={this.state.name} age={this.state.age} /><hr></hr><br></br>
+                <ChildComponent name={this.state.name} age={this.state.age} updateName={this.updateName} /><hr></hr><br></br>
             </div>
         )
     }
 }
 
-function ChildComponent(props) {
-    return (
-        <div>
-            <b id="nameValue">User Name is: {props.name}</b><br></br>
-            <OtherChildComponent age={props.age} />
-        </div>
-    )
+class ChildComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            age: 30
+        }
+    }
+
+    updateAge = () => {
+        this.setState({
+            age: 150
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <OtherChildComponent age={this.state.age} name={this.props.name} updateAge={this.updateAge} updateName={this.props.updateName}/>
+            </div>
+        )
+    } 
 }
 
 function OtherChildComponent(props) {
     return (
         <div>
             <b id="ageValue">User Age is: {props.age}</b><br></br>
+            <b id="nameValue">User Name is: {props.name}</b><br></br>
+            <input type="button" value="Update Age" onClick={props.updateAge} />
+            <input type="button" value="Update Name" onClick={props.updateName} />
         </div>
     )
 }
